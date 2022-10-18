202022e70f6fd9b769e1869b2ae10aac14bf2df5
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-10-18 13:13:06 +0530

    [Shivaraj] Add: added program to create and display a DataFrame from a specified dictionary data which has the index labels

---
+++ Pandas/program_5.py
@@ -0,0 +1,55 @@
+'''
+    @Author: Shivaraj
+    @Date: 13-10-2022
+    @Last Modified by: Shivaraj
+    @Last Modified date: 13-10-2022
+    @Title: Write a Python program to create and display a DataFrame from a specified dictionary
+            data which has the index labels.
+            Sample Python dictionary data and list labels:
+            exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael',
+            'Matthew', 'Laura', 'Kevin', 'Jonas'],
+            'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+            'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+            'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+            labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+'''
+import sys
+
+import numpy as np
+from data_log import get_logger
+
+import pandas as pd
+
+lg = get_logger(name="(program_5)", file_name="data_log.log")
+
+
+class PandasDataManipulation:
+
+    def display_dataframe(self, exam_data, labels):
+        """
+        Description:
+             This function is used to display a dataframe
+        Parameter:
+            exam_data and labels: The exam_data and labels to be checked
+        Return:
+            Returns None
+        """
+        try:
+            data_frame = pd.DataFrame(exam_data, index=labels)
+            lg.info(f" DataFrame: \n{data_frame}")
+        except Exception:
+            exception_type, _, exception_traceback = sys.exc_info()
+            line_number = exception_traceback.tb_lineno
+            lg.exception(
+                f"Exception type : {exception_type} \nError on line number : {line_number}")
+
+
+if __name__ == "__main__":
+    exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael',
+                          'Matthew', 'Laura', 'Kevin', 'Jonas'],
+                 'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+                 'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+                 'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+    labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    pd_obj = PandasDataManipulation()
+    pd_obj.display_dataframe(exam_data, labels)

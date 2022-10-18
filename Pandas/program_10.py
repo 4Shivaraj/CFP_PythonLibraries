c5adee215d6f89129e8fed2aeff2a39c43a4e437
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-10-18 13:20:28 +0530

    [Shivaraj] Add: added program to select the rows where the number of attempts in the examination is greater than 2

---
+++ Pandas/program_10.py
@@ -0,0 +1,58 @@
+'''
+    @Author: Shivaraj
+    @Date: 13-10-2022
+    @Last Modified by: Shivaraj
+    @Last Modified date: 13-10-2022
+    @Title: Write a Python program to select the rows where the number of attempts in the
+            examination is greater than 2.
+            exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew',
+            'Laura', 'Kevin', 'Jonas'],
+            'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+            'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+            'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+            labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+
+'''
+import sys
+
+from data_log import get_logger
+
+import numpy as np
+import pandas as pd
+
+lg = get_logger(name="(program_10)", file_name="data_log.log")
+
+
+class PandasDataManipulation:
+
+    def attempts_greater_than_two(self, exam_data, labels):
+        """
+        Description:
+            This function is used to display the rows where the number of attempts in the
+            examination is greater than 2.
+        Parameter:
+            exam_data and labels: The exam_data and labels to be checked
+        Return:
+            Returns None
+        """
+        try:
+            data_frame = pd.DataFrame(exam_data, index=labels)
+            lg.info(
+                f" Number of attempts in the examination is greater than 2: \n{data_frame[data_frame['attempts']>2]}")
+        except Exception:
+            exception_type, _, exception_traceback = sys.exc_info()
+            line_number = exception_traceback.tb_lineno
+            lg.exception(
+                f"Exception type : {exception_type} \nError on line number : {line_number}")
+
+
+if __name__ == "__main__":
+    exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael',
+                          'Matthew', 'Laura', 'Kevin', 'Jonas'],
+                 'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+                 'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+                 'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+    labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    pd_obj = PandasDataManipulation()
+    pd_obj.attempts_greater_than_two(exam_data, labels)

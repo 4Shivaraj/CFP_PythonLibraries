c92b1851024447381e0af488b5c3dcb27b6975fe
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-10-18 14:37:50 +0530

    [Shivaraj] Add: added program to plot two or more lines and set the line markers

---
+++ Matplotlib/Python_matplotlib/program_8.py
@@ -0,0 +1,71 @@
+'''
+    @Author: Shivaraj
+    @Date: 16-10-2022
+    @Last Modified by: Shivaraj
+    @Last Modified date: 16-10-2022
+    @Title: Write a Python program to plot two or more lines and set the line markers.
+'''
+import matplotlib.pyplot as plt
+import sys
+
+from data_log import get_logger
+
+lg = get_logger(name="(program_8)", file_name="data_log.log")
+
+
+class DrawLine:
+    """
+    class to perform graphical representation of data using matplotlib
+    """
+
+    def plot_two_or_more_lines(self):
+        """
+        Description:
+             This function is used to plot two or more lines and set the line markers.
+        Parameter:
+            None
+        Return:
+            Returns None
+        """
+        try:
+            # Line 1
+            x1 = [1, 2, 3]
+            y1 = [10, 20, 5]
+            lg.info(f'x1 range: {x1} and y1 range: {y1}')
+
+            # plotting the line 1 points with dash line style
+            plt.plot(x1, y1, color='blue', label='line 1', linestyle='dashed',
+                     marker='o', markerfacecolor='blue', markersize=12)
+
+            # Line 2
+            x2 = [1, 2, 3]
+            y2 = [40, 10, 5]
+            lg.info(f'x2 range: {x2} and y2 range: {y2}')
+
+            # plotting the line 2 points with dot line style
+            plt.plot(x2, y2, color='green', label='line 2', linestyle='dotted',
+                     marker='o', markerfacecolor='green', markersize=12)
+
+            # set label for axes
+            plt.xlabel('X axis')
+            plt.ylabel('y axis')
+
+            # title for plotting line
+            plt.title('Two or more lines on same plot with suitable legends ')
+
+            # show a legend on the plot
+            plt.legend()
+            
+            # show the figure
+            plt.show()
+
+        except Exception:
+            exception_type, _, exception_traceback = sys.exc_info()
+            line_number = exception_traceback.tb_lineno
+            lg.exception(
+                f"Exception type : {exception_type} \nError on line number : {line_number}")
+
+
+if __name__ == "__main__":
+    np_obj = DrawLine()
+    np_obj.plot_two_or_more_lines()

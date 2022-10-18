45e1b186c529c398e93fc74324a645eec473d667
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-10-18 14:42:33 +0530

    [Shivaraj] Add: added program to create multiple plots

---
+++ Matplotlib/Python_matplotlib/program_15.py
@@ -0,0 +1,74 @@
+'''
+    @Author: Shivaraj
+    @Date: 16-10-2022
+    @Last Modified by: Shivaraj
+    @Last Modified date: 16-10-2022
+    @Title: Write a Python program to create multiple plots.
+'''
+import matplotlib.pyplot as plt
+import sys
+
+from data_log import get_logger
+
+lg = get_logger(name="(program_15)", file_name="data_log.log")
+
+
+class DrawLine:
+    """
+    class to perform graphical representation of data using matplotlib
+    """
+
+    def multiple_plots(self):
+        """
+        Description:
+             This function is used to to create multiple plots.
+        Parameter:
+            None
+        Return:
+            Returns None
+        """
+        try:
+            # Line 1
+            x1 = [1, 2, 3]
+            y1 = [10, 20, 5]
+            lg.info(f'x1 range: {x1} and y1 range: {y1}')
+
+            # Sets up a subplot grid that has height 2 and width 1,
+            # and set the first such subplot as active.
+            plt.subplot(2, 1, 1)
+            # (1,1,1)=center , (2,2,1)=top left, (2,2,2)=top right, (2,2,3)=bottom left, (2,2,4)=bottom right
+
+            # plotting the line 1 points
+            plt.plot(x1, y1, label="line 1")
+
+            # Sets a title
+            plt.title('subplot1')
+
+            # Line 2
+            x2 = [1, 2, 3]
+            y2 = [40, 10, 5]
+            lg.info(f'x2 range: {x2} and y2 range: {y2}')
+
+            # Set the second subplot as active, and make the second plot.
+            plt.subplot(2, 1, 2)
+            # (1,1,1)=center , (2,2,1)=top left, (2,2,2)=top right, (2,2,3)=bottom left, (2,2,4)=bottom right
+
+            # plotting the line 2 points
+            plt.plot(x2, y2, label="line 2")
+
+            # Sets a title
+            plt.title('subplot2')
+
+            # Shows the figure.
+            plt.show()
+
+        except Exception:
+            exception_type, _, exception_traceback = sys.exc_info()
+            line_number = exception_traceback.tb_lineno
+            lg.exception(
+                f"Exception type : {exception_type} \nError on line number : {line_number}")
+
+
+if __name__ == "__main__":
+    np_obj = DrawLine()
+    np_obj.multiple_plots()

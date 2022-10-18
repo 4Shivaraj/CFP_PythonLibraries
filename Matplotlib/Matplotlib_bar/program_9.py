9c5ae2f9dd40ab3cdd1209166ec62c7081165410
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-10-18 15:11:00 +0530

    [Shivaraj] Add: added program to to display a bar chart of the popularity of programming Languages. Increase bottom margin

---
+++ Matplotlib/Matplotlib_bar/program_9.py
@@ -0,0 +1,73 @@
+'''
+    @Author: Shivaraj
+    @Date: 16-10-2022
+    @Last Modified by: Shivaraj
+    @Last Modified date: 16-10-2022
+    @Title: Write a Python programming to display a bar chart of the popularity of programming
+            Languages. Increase bottom margin.
+            Sample data:
+            Programming languages: Java, Python, PHP, JavaScript, C#, C++
+            Popularity: 22.2, 17.6, 8.8, 8, 7.7, 6.7
+'''
+import matplotlib.pyplot as plt
+import sys
+
+from data_log import get_logger
+
+lg = get_logger(name="(program_9)", file_name="data_log.log")
+
+
+class BarChart:
+    """
+    class to perform bar chart representation using matplotlib
+    """
+
+    def display_barchart(self):
+        """
+        Description:
+             This function is used to display a bar chart of the popularity of programming Languages
+        Parameter:
+            None
+        Return:
+            Returns None
+        """
+        try:
+            prg_lang = ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
+            popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+
+            x_pos = [i for i, _ in enumerate(prg_lang)]
+
+            # plotting x and y-axis values to create bar chart
+            plt.bar(x_pos, popularity, color=(0.4, 0.6, 0.8, 1.0))
+
+            # Set the x-axis label
+            plt.xlabel("Languages")
+
+            # Set the y-axis label
+            plt.ylabel("Popularity")
+
+            # Sets a title
+            plt.title("PopularitY of Programming Language\n" + "Worldwide, Oct 2017 compared to a year ago")
+
+            plt.xticks(x_pos, prg_lang, rotation=90)
+            plt.subplots_adjust(bottom=0.4, top=.8)
+            # to display minor ticks on the axes.
+            plt.minorticks_on()
+            plt.grid(which='major', linestyle='-',
+                     linewidth='0.5', color='red')
+            plt.grid(which='minor', linestyle=':',
+                     linewidth='0.5', color='black')
+
+            # To display graph
+            plt.show()
+
+        except Exception:
+            exception_type, _, exception_traceback = sys.exc_info()
+            line_number = exception_traceback.tb_lineno
+            lg.exception(
+                f"Exception type : {exception_type} \nError on line number : {line_number}")
+
+
+if __name__ == "__main__":
+    np_obj = BarChart()
+    np_obj.display_barchart()

ccb03e2acc8c553b3d88510bff91ba4cccf18cca
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-10-18 14:35:49 +0530

    [Shivaraj] Add: added program to plot two or more lines on same plot with suitable legends of each line

---
+++ Matplotlib/Python_matplotlib/program_5.py
@@ -0,0 +1,69 @@
+'''
+    @Author: Shivaraj
+    @Date: 16-10-2022
+    @Last Modified by: Shivaraj
+    @Last Modified date: 16-10-2022
+    @Title: Write a Python program to plot two or more lines on same plot with suitable legends of each line.
+'''
+import matplotlib.pyplot as plt
+import sys
+
+from data_log import get_logger
+
+lg = get_logger(name="(program_5)", file_name="data_log.log")
+
+
+class DrawLine:
+    """
+    class to perform graphical representation of data using matplotlib
+    """
+
+    def line_plotting(self):
+        """
+        Description:
+             This function is used to plot two or more lines on same plot with suitable legends of each line
+        Parameter:
+            None
+        Return:
+            Returns None
+        """
+        try:
+            # Line 1
+            x1 = [1, 2, 3, 4]
+            y1 = [5, 4, 1, 6]
+            lg.info(f'x1 range: {x1} and y1 range: {y1}')
+
+            # plot lines to the axes
+            plt.plot(x1, y1, label='Line 1')
+
+            # Line 2
+            x2 = [4, 2, 7, 3]
+            y2 = [2, 4, 9, 5]
+            lg.info(f'x2 range: {x2} and y2 range: {y2}')
+
+            # plot lines to the axes
+            plt.plot(x2, y2, label='Line 2')
+
+            # set label for axes
+            plt.xlabel('X axis')
+            plt.ylabel('Y axis')
+
+            # title for plotting line
+            plt.title('Two lines on same plot')
+
+            # show a legend on the plot
+            plt.legend()
+
+            # show the figure
+            plt.show()
+
+        except Exception:
+            exception_type, _, exception_traceback = sys.exc_info()
+            line_number = exception_traceback.tb_lineno
+            lg.exception(
+                f"Exception type : {exception_type} \nError on line number : {line_number}")
+
+
+if __name__ == "__main__":
+    np_obj = DrawLine()
+    np_obj.line_plotting_with_legends()
